e897a71d45e3daa7a63a6e13d5014aa06d72f577
unseen92 <81285576+unseen92@users.noreply.github.com>
2022-03-09 17:19:30 -0500

    Add project files.

---
+++ client/Main.cs
@@ -0,0 +1,35 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+
+namespace client
+{
+    public class Main : BaseScript
+    {
+      
+        public Main()
+        {
+            API.RegisterCommand("test", new Action(TestCommand), false);
+        }   
+
+        private async static void TestCommand()
+        {
+            Ped player = Game.Player.Character;
+            API.RequestModel((uint)PedHash.ChemSec01SMM);
+            while (!API.HasModelLoaded((int)PedHash.ChemSec01SMM))
+            {
+                Debug.WriteLine("Waiting for model to load");
+                await BaseScript.Delay(100);
+            }
+            Ped bodyguard = await World.CreatePed(PedHash.ChemSec01SMM, player.Position + (player.ForwardVector * 2));
+            bodyguard.Task.LookAt(player);
+            API.SetPedAsGroupMember(bodyguard.Handle, API.GetPedGroupIndex(player.Handle));
+            API.SetPedCombatAbility(bodyguard.Handle, 2);
+            API.GiveWeaponToPed(bodyguard.Handle,(uint)WeaponHash.AssaultRifleMk2, 999, false, true);
+            bodyguard.PlayAmbientSpeech("GENERIC_HI");
+
+        }
+
+
+    }
+}
